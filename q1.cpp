46f2773480804be25ba75d41e57a583d6e1f7815
AnsleighCarstens <agc0121@auburn.edu>
2024-10-21 11:39:15 -0500

    Update q1.cpp

--- q1.cpp
+++ q1.cpp
@@ -1,41 +1,69 @@
 #include <iostream>
-#include <iomanip>
+#include <cstdlib>
 
 using namespace std;
 
 class GasPump {
 	private:
-		double amountDispensed,
-		       amountCharged,
-		       costPerGallon;
+		//Private member variables
+		float pricePerGallon;
+		int gallons;
+		float amount;
 
 	public:
-		double getAmountDispensed() { return amountDispensed; }
-		double getAmountCharged() { return amountCharged; }
-		double getCostPerGallon() { return costPerGallon; }
-		void setCostPerGallon(double cpg) { costPerGallon = cpg; }
-        
-		void dispense(int seconds) {
-
-
-			
-			amountCharged = amountDispensed * costPerGallon;
+		// Constructor
+		GasPump(float pricePerGallon, int gallons, float amount)  {
+			this->pricePerGallon = pricePerGallon;
+			this->gallons = gallons;
+			this ->amount = amount;
 		}
+        
+		// Resets the values of pump display counters
 		void reset() {
-            
-			amountDispensed = 0.0;
-			amountCharged = 0.0;
-		}		
+			gallons = 0;
+			amount = 0;
+		}
+
+		// Resets the displays, dispenses gasoline, computes values, and displays values as gas is being pumped
+		void fill() {
+			reset();
+			int dispensed = quantity();
+			for (int i = 0; i <= dispensed; i++) {
+				display();
+				gallons += 1;
+				amount += pricePerGallon;
+			}
+		}
+
+		// Prints the pump displays - price per gallon, # of gallons, and total price
+		void display() {
+			cout << "Price: $" << pricePerGallon << " Gallons: " << gallons << " Amount: $" << amount << amount << endl;
+		}
+
+		// Call display to print the current display of the pump
+		void start_() {
+			display();
+		}
+
+		// Generate a random number to represent the amount of gasoline dispensed
+		int start_buy() {
+			display();
+	}		
 };
 	
 int main() {
+	int k;
+	cout << "Enter k (number of gas purchases to stimulate): ";
+	cin >> k ;
+
+	GasPump pump(3.15, 10, 31.50);
+
+	for (int i = 1; i <= k; i++) {
+		cout << i << ":" << endl;
+		pump.start_buy();
+		pump.fill();
+		cout << "End of fill." <<endl << endl;
+	}
 	
-    //Define variables
-
-    // Define Class Object
-
-    // Call Functions
-	
-
 	return 0;
 }
